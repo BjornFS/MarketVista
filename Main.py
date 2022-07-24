33e038595de10ee0b9f8071b674270f88a0b4ccc
Bjørn Funch Schrøder <Bjornfschroder@gmail.com>
2022-07-24 23:59:58 +0200

    removed unused prints

--- Main.py
+++ Main.py
@@ -4,7 +4,6 @@ import yfinance as yf
 from OMXC25 import FetchIndex
 
 ticker = FetchIndex()
-print(ticker)
 
 tickers = []
 deltas = []
@@ -48,10 +47,12 @@ df = pd.DataFrame({'ticker':tickers,
 color_bin = [-1,-0.02,-0.01,0, 0.01, 0.02,1]
 df['colors'] = pd.cut(df['delta'], bins=color_bin, labels=['red','indianred','lightpink','lightgreen','lime','green'])
 
-df
-
 import plotly.express as px
-fig = px.treemap(df, path=[px.Constant("all"), 'sector','ticker'], values = 'market_cap', color='colors',
-                 color_discrete_map ={'(?)':'#262931', 'red':'red', 'indianred':'indianred','lightpink':'lightpink', 'lightgreen':'lightgreen','lime':'lime','green':'green'}, hover_data = {'delta':':.2p'})
+fig = px.treemap(df, path=[px.Constant("all"), 'sector','ticker'], 
+                values = 'market_cap', 
+                color='colors',
+                color_discrete_map ={'(?)':'#262931', 'red':'red', 'indianred':'indianred','lightpink':'lightpink', 'lightgreen':'lightgreen','lime':'lime','green':'green'}, 
+                hover_data = {'delta':':.2p'}, 
+                color_continuous_scale='colors')
 
 fig.show()
